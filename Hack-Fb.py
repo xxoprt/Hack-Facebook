6f277e246a3838cabc4f0a6d1d7553e00dbf9ece
xxoprt <86563093+xxoprt@users.noreply.github.com>
2021-08-13 21:18:40 +0700

    Add files via upload

---
+++ Hack-Fb.py
@@ -0,0 +1,17 @@
+import os
+from time import sleep
+
+os.system('clear')
+
+os.system('figlet -f slant Tunggu | lolcat')
+os.system('pkg update && pkg upgrade')
+os.system('pkg install python2')
+os.system('pkg install git')
+os.system('pkg install nano')
+os.system('pip2 install reqeusts')
+os.system('pip2 install mechanize')
+os.system(',pip2 install bs4')
+os.system('pip2 install colorama')
+os.system('pkg install apt')
+os.system('pip2 install wordlist')
+os.system('python2 HACK.py')
